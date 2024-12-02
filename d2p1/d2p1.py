0ff13f11ffc1012ec584611fb9265a642eea297a
Michael Griffiths <michael.griffiths2@aon.com>
2024-12-02 16:01:20 +0000

    Updates d2p1 to align with d2p2.

--- d2p1/d2p1.py
+++ d2p1/d2p1.py
@@ -1,42 +1,29 @@
 import numpy as np
+from itertools import pairwise
 
 def d2p1(input):
     safe_reports = 0
 
     for i in range(len(input)):
-        safe = True
-        
         report = []
 
         report = list(map(int, input[i].split()))
 
-        l = 0
-        m = 1
-        r = 2
+        diffs = [y - x for x, y in pairwise(report)]
 
-        while m < len(report):
-            diff1 = report[l] - report[m]
-
-            if abs(diff1) < 1 or abs(diff1) > 3:
-                safe = False
-                continue
-
-            if r < len(report):
-                diff2 = report[m] - report[r]
-
-                if np.sign(diff1) != np.sign(diff2):
-                    safe = False
-                    continue
-
-            l += 1
-            m += 1
-            r += 1
-
-        if safe:
+        if check_diffs(diffs):
             safe_reports += 1
 
     return safe_reports
 
+def check_diffs(input = []):
+    if any(np.abs(input) == 0) or any(np.abs(input) > 3):
+        return False
+    
+    if all(np.sign(input) < 0) or all(np.sign(input) > 0):
+        return True
+    
+    return False
 
 if __name__ == '__main__':
     with open('./d2p1/input.txt') as f:
