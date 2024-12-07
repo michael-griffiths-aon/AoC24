915ba988a3c21257f3942f4d8d957d6f00a4d445
mtgriff34 <michaeltgriffiths34@gmail.com>
2024-12-07 16:29:00 +0000

    Refactor d6p1.py.

--- d6p1/d6p1.py
+++ d6p1/d6p1.py
@@ -1,78 +1,43 @@
-def d6p1(input):
-    move_guard(input)
-    
-    count = 0
-    for i in input:
-        for j in i:
-            if j == 'X':
-                count += 1
-
-    return count
-
-def find_guard(input):
+def locate_blocks(input):
+    blocks = []
     for i in range(len(input)):
         for j in range(len(input[i])):
-            if input[i][j] in ['^', '>', '<', 'v']:
-                return (i, j)
+            if input[i][j] == '#':
+                blocks.append(complex(j, i))
 
-def move_guard(input):
+    return blocks
+
+def locate_guard(input):
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            if input[i][j] == '^':
+                return complex(j, i)
+
+def guard_path(input):
     length = len(input)
     width = len(input[0])
-    total = length * width
-    check = False
-    position = find_guard(input)
-    while check == False:
-        # Up
-        if input[position[0]][position[1]] == '^':
-            if position[0] - 1 < 0:
-                input[position[0]][position[1]] = 'X'
-                check = True
-            elif input[position[0] - 1][position[1]] == '#':
-                input[position[0]][position[1]] = '>'
-                position = (position[0], position[1])
-            else:
-                input[position[0] - 1][position[1]] = '^'
-                input[position[0]][position[1]] = 'X'
-                position = (position[0] - 1, position[1])
-        # Right
-        elif input[position[0]][position[1]] == '>':
-            if width <= position[1] + 1:
-                input[position[0]][position[1]] = 'X'
-                check = True
-            elif input[position[0]][position[1] + 1] == '#':
-                input[position[0]][position[1]] = 'v'
-                position = (position[0], position[1])
-            else:
-                input[position[0]][position[1] + 1] = '>'
-                input[position[0]][position[1]] = 'X'
-                position = (position[0], position[1] + 1)
-        # Down
-        elif input[position[0]][position[1]] == 'v':
-            if length <= position[0] + 1:
-                input[position[0]][position[1]] = 'X'
-                check = True
-            elif input[position[0] + 1][position[1]] == '#':
-                input[position[0]][position[1]] = '<'
-                position = (position[0], position[1])
-            else:
-                input[position[0] + 1][position[1]] = 'v'
-                input[position[0]][position[1]] = 'X'
-                position = (position[0] + 1, position[1])
-        # Left
-        elif input[position[0]][position[1]] == '<':
-            if position[1] - 1 < 0:
-                input[position[0]][position[1]] = 'X'
-                check = True
-            elif input[position[0]][position[1] - 1] == '#':
-                input[position[0]][position[1]] = '^'
-                position = (position[0], position[1])
-            else:
-                input[position[0]][position[1] - 1] = '<'
-                input[position[0]][position[1]] = 'X'
-                position = (position[0], position[1] - 1)
+    guard_pos = locate_guard(input)
+    path = []
+    blocks = locate_blocks(input)
+    direction = complex(0, -1)
+    inrange = True
+    while inrange:
+        if guard_pos not in path:
+            path.append(guard_pos)
+        if (guard_pos + direction).imag < 0 or (guard_pos + direction).imag >= length or (guard_pos + direction).real < 0 or (guard_pos + direction).real >= width:
+            inrange = False
+        if guard_pos + direction in blocks:
+            direction *= 1j
+            guard_pos += direction
+        else:
+            guard_pos += direction
 
-    return input
+    return path
 
+def d6p1(input):
+    path = guard_path(input)
+
+    return len(path)
 
 if __name__ == '__main__':
     with open('./d6p1/input.txt') as f:
@@ -81,3 +46,4 @@ if __name__ == '__main__':
     for i in raw:
         input.append(list(i.strip()))
     print(d6p1(input))
+                
