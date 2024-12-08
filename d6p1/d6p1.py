3fc5d5604a43007faf2821caafef132f1674631a
mtgriff34 <michaeltgriffiths34@gmail.com>
2024-12-08 11:14:17 +0000

    Refactor d6p1.py.

--- d6p1/d6p1.py
+++ d6p1/d6p1.py
@@ -1,49 +1,30 @@
-def locate_blocks(input):
-    blocks = []
-    for i in range(len(input)):
-        for j in range(len(input[i])):
-            if input[i][j] == '#':
-                blocks.append(complex(j, i))
-
-    return blocks
-
-def locate_guard(input):
-    for i in range(len(input)):
-        for j in range(len(input[i])):
-            if input[i][j] == '^':
-                return complex(j, i)
-
-def guard_path(input):
-    length = len(input)
-    width = len(input[0])
-    guard_pos = locate_guard(input)
-    path = []
-    blocks = locate_blocks(input)
-    direction = complex(0, -1)
-    inrange = True
-    while inrange:
-        if guard_pos not in path:
-            path.append(guard_pos)
-        if (guard_pos + direction).imag < 0 or (guard_pos + direction).imag >= length or (guard_pos + direction).real < 0 or (guard_pos + direction).real >= width:
-            inrange = False
-        if guard_pos + direction in blocks:
-            direction *= 1j
-            guard_pos += direction
-        else:
-            guard_pos += direction
-
-    return path
-
-def d6p1(input):
-    path = guard_path(input)
-
-    return len(path)
-
 if __name__ == '__main__':
     with open('./d6p1/input.txt') as f:
-        raw = list(f.readlines())
-    input = []
-    for i in raw:
-        input.append(list(i.strip()))
-    print(d6p1(input))
+        input = f.readlines()
+
+    range = {j + i * 1j: c  for i,r in enumerate(input)
+                            for j,c in enumerate(r.strip())}
+    
+    def get_start_pos():
+        for position, state in range.items():
+            if state == '^':
+                return position
+
+    def guard_path(domain):
+        guard_pos = get_start_pos()
+        direction = -1j
+        path = [] 
+        while guard_pos in domain:
+            if domain.get(guard_pos + direction) == '#':
+                direction *= 1j
+                guard_pos += direction    
+            else:
+                guard_pos += direction
+            if guard_pos not in path:
+                path.append(guard_pos)
+        return path
+
+    path = guard_path(range) 
+
+    print(len(path))  
                 
